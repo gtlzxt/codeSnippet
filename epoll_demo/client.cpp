36452db26b07878069b112db9c5826141d64eb0c
Tony Guo <343994664@qq.com>
2019-11-26 14:23:11 +0800

    add comment for client

--- epoll_demo/client.cpp
+++ epoll_demo/client.cpp
@@ -13,6 +13,7 @@
 #define MAX_LINE (1024)
 #define SERVER_PORT (7778)
 
+//设置nonblocking
 void setnoblocking(int fd)
 {
     int opts = 0;
@@ -21,18 +22,21 @@ void setnoblocking(int fd)
     fcntl(fd, F_SETFL);
 }
 
+
 void client(char* serverIp)
 {
     int sockfd;
     char recvline[MAX_LINE + 1] = {0};
     struct sockaddr_in server_addr;
-
+	
+    //创建socket
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
         perror("socket error");
         return;
     }
 
+    //配置socket
     bzero(&server_addr, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(SERVER_PORT);
@@ -43,12 +47,14 @@ void client(char* serverIp)
         return;
     }
 
+    //连接server
     if(connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
     {
         fprintf(stderr, "connect error\n");
         return;
     }
 
+    //设置非阻塞
     setnoblocking(sockfd);
 
     char input[100];
@@ -59,6 +65,7 @@ void client(char* serverIp)
     {
         printf("[send] %s\n", input);
         n = 0;
+	//向server发送数据
         n = send(sockfd, input, strlen(input), 0);
         if(n < 0)
         {
@@ -70,6 +77,7 @@ void client(char* serverIp)
 
         while(1)
         {
+	    //读取数据
             n = read(sockfd, recvline + count, MAX_LINE);
             if(n == MAX_LINE)
             {
